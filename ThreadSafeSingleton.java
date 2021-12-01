a2552cd5bb9ac9dc3c70cdc2ea6f22a3d35695c9
omkumar7 <91427638+omkumar7@users.noreply.github.com>
2021-12-01 18:10:37 +0530

    Add files via upload

---
+++ ThreadSafeSingleton.java
@@ -0,0 +1,13 @@
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton()
+    {}
+
+    synchronized public static ThreadSafeSingleton getInstance()
+    {
+        if (instance == null)
+        {instance = new ThreadSafeSingleton();}
+        return instance;
+    }
+}
